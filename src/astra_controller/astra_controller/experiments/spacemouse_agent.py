fcca77c8089406ac3d76d417dd8bf29aff4e08d9
dong_home <dongje.musk.lee@gmail.com>
2025-05-13 19:24:02 +0900

    remove experience's spacemouse

--- src/astra_controller/astra_controller/experiments/spacemouse_agent.py
+++
@@ -1,9 +0,0 @@
-from astra_controller.experiments.spacemouse_expert import SpaceMouseExpert
-
-class SpacemouseAgent():
-    def __init__(self):
-        self.mouse = SpaceMouseExpert()
-        
-    def act(self, observation):
-        action, buttons = self.mouse.get_action()
-        return action, buttons
